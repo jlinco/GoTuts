7cc8832cf4de2516f3ce370e0a2c668097e233e3
Joel Cofie <jtcofie@gmail.com>
2018-05-14 16:54:40 +0000

    `main.go` updated

--- cards/main.go
+++ cards/main.go
@@ -1,18 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	// fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.print()
 }
